4881f0ffe66b857ea6c278eb64e11cc3f67237c0
Talha Turgut <talhaturgut@windowslive.com>
2020-11-12 11:56:09 +0300

    drawer ve appbar kod düzeltmesi yapıldı.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -38,180 +38,155 @@ class Homepage extends State<Stateee> {
   }
 
   Widget page1() {
-    return Scaffold(
-      key: scaffold,
-      drawer: Drawer(child: DrawerBuilder()),
-      appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              scaffold.currentState.openDrawer();
-            }),
-        title: Text("Learn English Words"),
-      ),
-      body: Container(
-          child: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (
-          context,
-          index,
-        ) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(_borderRadius),
-                      gradient: LinearGradient(
-                        colors: [Color(0xff6DC8F3), Color(0xff73A1F9)],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
+    return Container(
+        child: ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (
+        context,
+        index,
+      ) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(_borderRadius),
+                    gradient: LinearGradient(
+                      colors: [Color(0xff6DC8F3), Color(0xff73A1F9)],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xff73A1F9),
+                        blurRadius: 12,
+                        offset: Offset(0, 6),
                       ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xff73A1F9),
-                          blurRadius: 12,
-                          offset: Offset(0, 6),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  top: 0,
+                  child: CustomPaint(
+                    size: Size(100, 150),
+                    painter: CustomCardShapePainter(
+                        _borderRadius, Color(0xff6DC8F3), Color(0xff73A1F9)),
+                  ),
+                ),
+                Positioned.fill(
+                    child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Image.asset(
+                          items[index].image,
+                          height: 64,
+                          width: 64,
                         ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    bottom: 0,
-                    top: 0,
-                    child: CustomPaint(
-                      size: Size(100, 150),
-                      painter: CustomCardShapePainter(
-                          _borderRadius, Color(0xff6DC8F3), Color(0xff73A1F9)),
-                    ),
-                  ),
-                  Positioned.fill(
-                      child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: Image.asset(
-                            items[index].image,
-                            height: 64,
-                            width: 64,
-                          ),
-                          flex: 2),
-                      Expanded(
-                        flex: 4,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              items[index].name,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'Avenir',
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                            Text(
-                              items[index].des,
-                              style: TextStyle(
+                        flex: 2),
+                    Expanded(
+                      flex: 4,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            items[index].name,
+                            style: TextStyle(
                                 color: Colors.white,
                                 fontFamily: 'Avenir',
+                                fontSize: 25,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            items[index].des,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Avenir',
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.location_on,
+                                  color: Colors.white, size: 16),
+                              SizedBox(
+                                width: 8,
                               ),
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Icon(Icons.location_on,
-                                    color: Colors.white, size: 16),
-                                SizedBox(
-                                  width: 8,
-                                ),
-                                Text(
-                                  'Some title text here',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Avenir',
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Text(
-                              '4.7',
-                              style: TextStyle(
+                              Text(
+                                'Some title text here',
+                                style: TextStyle(
                                   color: Colors.white,
                                   fontFamily: 'Avenir',
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            Icon(
-                              Icons.star,
-                              color: Colors.white,
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ))
-                ],
-              ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            '4.7',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Avenir',
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ))
+              ],
             ),
-          );
-        },
-      )),
-    );
+          ),
+        );
+      },
+    ));
   }
 
   Widget page2() {
-    return Scaffold(
-      key: scaffold,
-      drawer: Drawer(child: DrawerBuilder()),
-      appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              scaffold.currentState.openDrawer();
-            }),
-        title: Text("Learn English Words"),
-      ),
-      body: Container(
-          child: Center(
-        child: Text("Profil"),
-      )),
-    );
+    return Container(
+        child: Center(
+      child: Text("Profil"),
+    ));
   }
 
   Widget page3() {
-    return Scaffold(
-      key: scaffold,
-      drawer: Drawer(child: DrawerBuilder()),
-      appBar: AppBar(
-        title: Text("Learn English Words"),
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              scaffold.currentState.openDrawer();
-            }),
-      ),
-      body: Container(
-          child: Center(
-        child: Text("En iyi"),
-      )),
-    );
+    return Container(
+        child: Center(
+      child: Text("En iyi"),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffold,
+      drawer: Drawer(
+        child: DrawerBuilder(),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              scaffold.currentState.openDrawer();
+            }),
+      ),
       resizeToAvoidBottomInset: false,
       body: showPage(_selectedIndex),
       floatingActionButton: FloatingActionButton(
